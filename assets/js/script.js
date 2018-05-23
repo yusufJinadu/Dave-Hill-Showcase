a32615bc8774b0104bfccb93945f076bb82fadfe
yusufJinadu <y_jinadu@yahoo.com>
2018-05-23 18:41:49 +0100

    added login

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,6 @@
 $(".img-thumbnail").on("click" , function(){
     $(this).toggleClass("enlarged")
 })
+$(".message a").click(function(){
+    $("form").animate({ height:"toggle", opacity: "toggle"}, "slow")
+})
