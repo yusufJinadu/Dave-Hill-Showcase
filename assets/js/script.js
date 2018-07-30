f20bb762289af67618627aca6de5aa01df358688
yusufJinadu <y_jinadu@yahoo.com>
2018-07-30 22:03:03 +0100

    new commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@
 $(".img-thumbnail").on("click" , function(){
     $(this).toggleClass("enlarged")
 })
-$(".message a").click(function(){
+/*$(".message a").click(function(){
     $("form").animate({ height:"toggle", opacity: "toggle"}, "slow")
+})*/
+$(".message").click(function(){
+    $(".register-form").toggleClass("not-show")
+    $(".login-form").toggleClass("not-show")
 })
